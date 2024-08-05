a3598f9bc25edae18a687708dd798976dfb4ba61
Aayush <rajkarnicar@gmail.com>
2024-08-05 23:21:02 +0545

    switch button fixed

--- bus-ticket-booking/src/Search-bar.jsx
+++ bus-ticket-booking/src/Search-bar.jsx
@@ -1,17 +1,29 @@
 import styles from "./index.module.css";
+import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 import { useState } from "react";
 function Searchbar(){
-    const [date,setdate]=useState('');
-    const currdate=new Date;
+    const [destination,setdestination]=useState('');
+    const [pickup,setpickup]=useState('');
+    function handle_switch_click(){
+        setdestination(des=>pickup);
+        setpickup(pic=>destination);
+    }
+    function pickupchange(e){
+        setpickup(e.target.value);
+    }
+    function destinationchange(e){
+        setdestination(e.target.value);
+    }
     return(
         <>
             <div className={styles.searchbar}>
-                <form action="" className={styles.form} onSubmit={console.log("hellow")}>
-                    <input type="text" placeholder="Pickup"></input>
-                    <input type="text" placeholder="Destination"/>
-                    <input type="date" />
-                    <button  className={styles.btnsubmit}>Submit</button>
-                </form>
+                <div className={styles.form} >
+                    <input type="text" placeholder="Pickup" value={pickup} onChange={pickupchange}/>
+                    <button className={styles.btn} onClick={handle_switch_click}><HiOutlineSwitchHorizontal/></button>       
+                    <input type="text" placeholder="Destination" value={destination} onChange={destinationchange}/>
+                    <input type="date"/>
+                    <button  className={styles.btn}>Submit</button>
+                </div>
             </div>
         </>
     );
